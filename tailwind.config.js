ddfc44240bba7887d660edc07fc5bd83c07da173
Dobrado <sdobrado@gmail.com>
2019-08-14 20:33:48 -0300

    add fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,24 @@
  ** Docs: https://tailwindcss.com/docs/configuration
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  theme: {},
+  theme: {
+    fontFamily: {
+      body: ['"Press Start 2P"', ...defaultTheme.fontFamily.sans]
+    },
+    extend: {
+      colors: {
+        warning: '#e64a16',
+        primary: '#002642',
+        secondary: '#f86d13',
+        danger: '#840032',
+        white: '#fcfbfa',
+        black: '#02040f'
+      }
+    }
+  },
   variants: {},
   plugins: []
 }
