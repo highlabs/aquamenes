8439e46bcd51f22c07f5a14a45f5d326fb6f2905
Dobrado <sdobrado@gmail.com>
2019-08-14 20:36:48 -0300

    Number input

---
+++ components/NumberInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <input
+    class="border-2 border-white bg-black p-4 my-2 inline-block"
+    type="number"
+    :value="value"
+    required
+    @input="handleInput"
+  />
+</template>
+
+<script>
+export default {
+  name: 'InputNumber',
+  model: {
+    event: 'modified'
+  },
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    handleInput(newVal) {
+      this.$emit('modified', parseFloat(newVal.target.value))
+    }
+  }
+}
+</script>
