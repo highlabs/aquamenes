c2163c28741611382fa068f63055b80f474f1741
Dobrado <sdobrado@gmail.com>
2019-08-14 20:36:42 -0300

    Subtitle component

---
+++ components/Subtitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-2xl font-bold pb-4 pt-2">{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Subtitle',
+  props: {
+    title: {
+      type: String,
+      default: 'Aquamenes'
+    }
+  }
+}
+</script>
