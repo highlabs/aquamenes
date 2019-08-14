cca65563c221e8e3bc34ade954695ee8b6fbc713
Dobrado <sdobrado@gmail.com>
2019-08-14 20:34:53 -0300

    Add font

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
+  <main class="px-4 bg-black min-h-screen text-white">
     <nuxt />
-  </div>
+  </main>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap');
+
+body {
+  @apply font-body;
+}
+
+p {
+  @apply text-sm my-2;
+}
+</style>
